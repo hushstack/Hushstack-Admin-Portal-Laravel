7c420c9517ce43c25c7646200e0a95cfe619b54d
hongsomonor <somonorhong011@gmail.com>
2026-02-24 22:05:05 +0700

    sdsd

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://portal.hushstackcambodia.site',
+        'https://portfolio.hushstackcambodia.site',
+    ],
 
     'allowed_origins_patterns' => [],
 
